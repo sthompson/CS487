1b55ecfe881dc308a8e64a5bce678e2c79f575ef
Seth Thompson <sthomp12@iit.edu>
2012-03-10 17:25:49 -0600

    Attempting to get menu

--- hellodjango/restaurants/views.py
+++ hellodjango/restaurants/views.py
@@ -9,7 +9,7 @@ from hellodjango.restaurants.models import Food_Item
 
 def json(request):
    data = serializers.serialize("json",Restaurant.objects.all())
-   return HttpResponse(simplejson.dumps(data))
+   return HttpResponse(data)
 
 def restaurants(request):
    data = serializers.serialize("python",Restaurant.objects.all())
@@ -18,7 +18,8 @@ def restaurants(request):
 
 def menu(request,res_name): 
    restaurant = serializers.serialize("python",Menu.objects.filter(name=res_name))
-   data = serializers.serialize("python",Menu.objects.filter(name=res_name))
+   res_id = restaurant[0]["pk"]
+   data = serializers.serialize("python",Menu.objects.filter(restaurant=res_id))
    actual_data = [d["fields"] for d in data]
    return HttpResponse(simplejson.dumps(actual_data))
    
