8b20b0a28731d5e52a25dbdb408e7367bb58420f
Seth Thompson <sthomp12@iit.edu>
2012-03-10 16:21:07 -0600

    removed traces of restaurant

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-   'hellodjango.restaurants',
+#   'hellodjango.restaurants',
    'south',
 )
 
