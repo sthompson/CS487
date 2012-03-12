5762a78933eb46a8f047888ad48e4b851ddb7660
Seth Thompson <sthomp12@iit.edu>
2012-03-11 22:00:59 -0500

    Erased invalid database info

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -16,9 +16,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'herokudb',                      # Or path to database file if using sqlite3.
-        'USER': 'seththompson',                      # Not used with sqlite3.
-        'PASSWORD': 'ilikecows',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
