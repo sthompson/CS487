bbb41021b47c121f57d4e4d2b5c7995a88e37977
Seth Thompson <sthomp12@iit.edu>
2012-03-21 14:48:41 -0500

    Added admin site

--- Back_End_Prototype/hellodjango/urls.py
+++ Back_End_Prototype/hellodjango/urls.py
@@ -3,8 +3,8 @@ from hellodjango.views import root
 from hellodjango.restaurants import views as res_views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
    (r'^$',root), 
@@ -20,5 +20,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
