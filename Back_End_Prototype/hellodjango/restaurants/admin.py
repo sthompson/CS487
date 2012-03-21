6ad10bd55cc2fe3be91521d4ed4a39382096d92b
Seth Thompson <sthomp12@iit.edu>
2012-03-21 14:50:53 -0500

    Registered models

---
+++ Back_End_Prototype/hellodjango/restaurants/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from hellodjango.restaurants.models import Restaurant, Food_Item, Menu
+
+admin.site.register(Restaurant)
+admin.site.register(Author)
+admin.site.register(Book)
