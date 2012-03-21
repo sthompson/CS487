0bc8d526b023f7265b206b310d3350d2c690e093
Seth Thompson <sthomp12@iit.edu>
2012-03-21 14:54:21 -0500

    fixed mistake in registering food items and menus

--- Back_End_Prototype/hellodjango/restaurants/admin.py
+++ Back_End_Prototype/hellodjango/restaurants/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from hellodjango.restaurants.models import Restaurant, Food_Item, Menu
 
 admin.site.register(Restaurant)
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Food_Item)
+admin.site.register(Menu)
