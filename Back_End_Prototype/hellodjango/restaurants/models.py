caa61b1a98a65090bf869228db6f1c7644b934c9
Seth Thompson <sthomp12@iit.edu>
2012-03-21 15:04:12 -0500

    Added names to models

--- Back_End_Prototype/hellodjango/restaurants/models.py
+++ Back_End_Prototype/hellodjango/restaurants/models.py
@@ -4,12 +4,21 @@ class Restaurant(models.Model):
    name = models.CharField(max_length=100)
    delivery = models.CharField(max_length=30)
    logoURL = models.CharField(max_length=200)
+   
+   def __unicode__(self):
+      return self.name
 
 class Food_Item(models.Model):
    name = models.CharField(max_length=100)
    itemURL = models.CharField(max_length=200)
    price = models.CharField(max_length=20)
 
+   def __unicode__(self):
+      return self.name
+
 class Menu(models.Model):
    restaurant = models.ForeignKey(Restaurant)
    food_item = models.ForeignKey(Food_Item) 
+
+   def __unicode__(self):
+      return restaurant.name + " " + food_item.name
