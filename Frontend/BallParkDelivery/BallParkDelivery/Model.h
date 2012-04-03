bf43edc75d34625db50d5eb7f18fa8b96bded636
Seth Thompson <sthomp12@iit.edu>
2012-04-02 22:17:41 -0500

    Added model class

---
+++ Frontend/BallParkDelivery/BallParkDelivery/Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  BallParkDelivery
+//
+//  Created by Seth  Thompson on 4/2/12.
+//  Copyright (c) 2012 Bradley University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Model : UIViewController
+
+@end
