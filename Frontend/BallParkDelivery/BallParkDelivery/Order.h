727de23fe3c5011a545cb15bb50bc420dd3f5787
Seth Thompson <sthomp12@iit.edu>
2012-04-05 20:55:40 -0500

    Added order class

---
+++ Frontend/BallParkDelivery/BallParkDelivery/Order.h
@@ -0,0 +1,13 @@
+//
+//  Order.h
+//  BallParkDelivery
+//
+//  Created by Seth  Thompson on 4/5/12.
+//  Copyright (c) 2012 Bradley University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Order : NSObject
+
+@end
---
+++ Frontend/BallParkDelivery/BallParkDelivery/Order.m
@@ -0,0 +1,13 @@
+//
+//  Order.m
+//  BallParkDelivery
+//
+//  Created by Seth  Thompson on 4/5/12.
+//  Copyright (c) 2012 Bradley University. All rights reserved.
+//
+
+#import "Order.h"
+
+@implementation Order
+
+@end
