58b22b2892544a79f538754b034c485867d22b23
Seth Thompson <sthomp12@iit.edu>
2012-04-06 09:57:18 -0500

    Added core data to the project

--- Frontend/BallParkDelivery/BallParkDelivery/AppDelegate.h
+++ Frontend/BallParkDelivery/BallParkDelivery/AppDelegate.h
@@ -14,4 +14,11 @@
 
 @property (strong, nonatomic) UINavigationController *navigationController;
 
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
 @end
