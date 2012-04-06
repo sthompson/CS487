d144377de3501d6395ef35b7378accfd73056958
Seth Thompson <sthomp12@iit.edu>
2012-04-05 19:53:24 -0500

    Added custom cell class

---
+++ Frontend/BallParkDelivery/BallParkDelivery/CustomCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomCell.h
+//  BallParkDelivery
+//
+//  Created by Seth  Thompson on 4/5/12.
+//  Copyright (c) 2012 Bradley University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCell : UITableViewCell
+
+@end
---
+++ Frontend/BallParkDelivery/BallParkDelivery/CustomCell.m
@@ -0,0 +1,29 @@
+//
+//  CustomCell.m
+//  BallParkDelivery
+//
+//  Created by Seth  Thompson on 4/5/12.
+//  Copyright (c) 2012 Bradley University. All rights reserved.
+//
+
+#import "CustomCell.h"
+
+@implementation CustomCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
