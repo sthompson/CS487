0a56d66dbfcae037ef6b23997cd96481c261d07d
Seth Thompson <sthomp12@iit.edu>
2012-04-05 23:38:40 -0500

    Created custom cell for cart

---
+++ Frontend/BallParkDelivery/BallParkDelivery/CartCell.m
@@ -0,0 +1,29 @@
+//
+//  CartCell.m
+//  BallParkDelivery
+//
+//  Created by Seth  Thompson on 4/5/12.
+//  Copyright (c) 2012 Bradley University. All rights reserved.
+//
+
+#import "CartCell.h"
+
+@implementation CartCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
