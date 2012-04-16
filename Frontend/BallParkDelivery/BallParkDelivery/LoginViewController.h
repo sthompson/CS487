a6c396a10f3a784fd87f2497bd50ccffb489356d
Seth Thompson <sthomp12@iit.edu>
2012-04-16 11:09:24 -0500

    Added login

---
+++ Frontend/BallParkDelivery/BallParkDelivery/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  BallParkDelivery
+//
+//  Created by Seth  Thompson on 4/16/12.
+//  Copyright (c) 2012 Bradley University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
