72a40c4304f7b89910f3a35b05fa9ee4f59b01ad
Seth Thompson <sthomp12@iit.edu>
2012-04-23 01:42:37 -0500

    removed backend prototype

--- Back_End_Prototype/hellodjango/restaurants/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from hellodjango.restaurants.models import Restaurant, Food_Item, Menu
-
-admin.site.register(Restaurant)
-admin.site.register(Food_Item)
-admin.site.register(Menu)
