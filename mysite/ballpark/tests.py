3f452d347ff23477bf0fa175bb2b8e39a990e339
Leroy Ng <leroyng7@gmail.com>
2012-04-24 15:45:04 -0500

    test

--- mysite/ballpark/tests.py
+++ mysite/ballpark/tests.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-=======
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -415,4 +397,3 @@ class SimpleTest(TestCase):
       self.assertEqual(response.status_code, 200)
       self.assertContains(response, "Cooking")
 
->>>>>>> 88673d18649e58e652b123c6b0f18065ece22be1
